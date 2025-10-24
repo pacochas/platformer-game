9669c704dc35b1a62568562f153b81587dd7134c
pacochas <pacochas@msoe.edu>
2025-10-24 11:15:59 -0500

    First working screen

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 import pygame
 import sys
 
+pygame.init()
+screen = pygame.display.set_mode((640, 480)) # initialize screen that's 640x480
+
+clock = pygame.time.Clock() # declare clock for framerate
+
+while True:
+    for event in pygame.event.get(): #user input so window doesn't freeze
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+    
+    
+    pygame.display.update()
+    clock.tick(60) # set fps to 60 
+    
+    
