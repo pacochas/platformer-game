0e8ba37ce7cb0b32047f4bcc980ba5ba3b37ccf6
pacochas <pacochas@msoe.edu>
2025-10-24 11:46:17 -0500

    Move cloud and add transparency color key

--- game.py
+++ game.py
@@ -10,15 +10,32 @@ class Game:
 
         self.clock = pygame.time.Clock() # declare clock for framerate
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-    
+        self.img.set_colorkey((0, 0, 0)) #transparent color key
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 200)) # display cloud
+            self.screen.fill((14, 219, 248)) #fill screen with blue sky
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5 # only allows for one direction if both keys are pressed
+            self.screen.blit(self.img, self.img_pos) # display cloud, top left is (0, 0)
+            
+            
             for event in pygame.event.get(): #user input so window doesn't freeze
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.exit
-            
+                    sys.exit()
+                if event.type == pygame.KEYDOWN: # check if user pressed down on a key
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP: # check if user releases key
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                    
+                    
             pygame.display.update()
             self.clock.tick(60) # set fps to 60 
     
