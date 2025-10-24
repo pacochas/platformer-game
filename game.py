61af45c237f66ca50e2ac4f2a689ee03877b9283
pacochas <pacochas@msoe.edu>
2025-10-24 11:29:37 -0500

    sprites added and cloud is displayed

--- game.py
+++ game.py
@@ -1,19 +1,26 @@
 import pygame
 import sys
 
-pygame.init()
-screen = pygame.display.set_mode((640, 480)) # initialize screen that's 640x480
+class Game:
+    def __init__(self):
+        pygame.init() # initializes pygame
+        
+        pygame.display.set_caption('Platformer')
+        self.screen = pygame.display.set_mode((640, 480)) # initialize screen that's 640x480
 
-clock = pygame.time.Clock() # declare clock for framerate
-
-while True:
-    for event in pygame.event.get(): #user input so window doesn't freeze
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit
+        self.clock = pygame.time.Clock() # declare clock for framerate
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
     
+    def run(self):
+        while True:
+            self.screen.blit(self.img, (100, 200)) # display cloud
+            for event in pygame.event.get(): #user input so window doesn't freeze
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
+            
+            pygame.display.update()
+            self.clock.tick(60) # set fps to 60 
     
-    pygame.display.update()
-    clock.tick(60) # set fps to 60 
-    
+Game().run()
     
